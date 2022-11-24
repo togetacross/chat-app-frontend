28c53c7d0d6ec718ef658231fe61f5e2801f4c9c
Gerdan Tibor <lionhearttibor@gmail.com>
2022-11-24 22:39:42 +0100

    https added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 //export const BASE_API_URL = 'http://localhost:8080/chatapp';
 //export const BASE_API_URL = 'http://161.35.205.45:3000/chatapp';
-export const BASE_API_URL = 'http://161.35.205.45/chatapp';
+export const BASE_API_URL = 'https://fruti2022.hu/chatapp';
